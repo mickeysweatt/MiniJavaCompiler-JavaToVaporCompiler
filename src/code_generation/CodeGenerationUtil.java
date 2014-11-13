5066b6fce1303b68346b24cbaa2f35b38505e5ac
Mickey Sweatt <mickeysweatt@engineering.ucla,edu>
2014-11-12 22:45:40 -0800

    Reorganized vtales

--- src/code_generation/CodeGenerationUtil.java
+++ src/code_generation/CodeGenerationUtil.java
@@ -10,12 +10,15 @@ import java.util.Map;
  * Created by admin on 11/10/14.
  */
 public class CodeGenerationUtil {
+    public static String vtableLabel(VaporClass c) {
+        return String.format("const %s.VTable", c.getName());
+    }
     public static void outputVtables(Environment env) {
         // TODO add subtyping
         for (Map.Entry<String, VaporClass> class_entry : env.getClasses().entrySet()) {
-            System.out.println(String.format("const %s.VTable", class_entry.getKey()));
+            System.out.println(vtableLabel(class_entry.getValue()));
             for (Map.Entry<String, MethodType> method_entry: class_entry.getValue().getMethods()) {
-                System.out.println(String.format(":%s", method_entry.getValue().getLabel()));
+                System.out.println(String.format("\t:%s", method_entry.getValue().getLabel()));
             }
         }
     }
